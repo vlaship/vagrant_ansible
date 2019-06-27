0121dcf9d5d89db4e2b79e039ffcfd3e886d0174
vlaship <vlashipby@gmail.com>
2019-06-27 22:19:22 +0300

    a

---
+++ vagrant/r1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+vagrant halt server-01
+vagrant destroy -f server-01
+vagrant up server-01
---
+++ vagrant/r2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+vagrant halt server-02
+vagrant destroy -f server-02
+vagrant up server-02
